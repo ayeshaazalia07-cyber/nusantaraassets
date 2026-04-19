ea67dba203a7c024371b14c0f28eea4d5f5ff27b
JWR'S <ayeshaazalia24@gmail.com>
2026-04-19 19:02:31 +0800

    Update: Menambahkan izin domain gambar Supabase dan Firebase di next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   allowedDevOrigins: ["192.168.1.8"],
+
+  images: {
+    remotePatterns: [
+      {
+        // 1. Izin untuk Supabase (Katalog Produk)
+        protocol: "https",
+        hostname: "ecmtooixzruszxbggsas.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        // 2. Izin untuk Firebase Storage kamu (Bukti Pembayaran)
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/nusantaraassets-5.firebasestorage.app/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
